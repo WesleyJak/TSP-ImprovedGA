82b179695539b9d2ab04f0a3c5b4d9819138d00b
JakA <jwesley1@unm.edu>
2024-04-23 16:22:06 -0600

    add class values

--- graph.py
+++ graph.py
@@ -1,6 +1,18 @@
 class Graph():
-    def __init__(self):
+    def __init__(self, nodes, complete):
+        self.nodes = nodes
+        self.complete = complete
+
+    def get_num_nodes(self):
+        return self.nodes
+    
+    def show(self):
         return
 
-def generate_graph():
-    return
+
+# Take a number of nodes and an option of complete or noncomplete. Generate the graph and return it.
+# Either complete or noncomplete will be an adjacency matrix
+def generate_graph(nodes, complete=True):
+    graph = Graph(nodes, complete)
+    return graph
+
